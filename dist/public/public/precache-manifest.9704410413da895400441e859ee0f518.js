27d292a06c143156e61bcb1c170cfb83c5bdd497
Phan Minh Dương <phanminhduong@hotmail.com>
2018-11-12 22:44:03 +0700

    update code

---
+++ dist/public/public/precache-manifest.9704410413da895400441e859ee0f518.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static1/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bc3729c40d83c9841bc5",
+    "url": "/static1/js/main.bc3729c4.chunk.js"
+  },
+  {
+    "revision": "04421cd3eea008be8224",
+    "url": "/static1/js/1.04421cd3.chunk.js"
+  },
+  {
+    "revision": "bc3729c40d83c9841bc5",
+    "url": "/static1/css/main.cef774b8.chunk.css"
+  },
+  {
+    "revision": "04421cd3eea008be8224",
+    "url": "/static1/css/1.73e893b4.chunk.css"
+  },
+  {
+    "revision": "e2e2ab37e2cec46a1590e4b49510fd76",
+    "url": "/index.html"
+  }
+];
--- public/precache-manifest.9704410413da895400441e859ee0f518.js
+++ public/precache-manifest.9704410413da895400441e859ee0f518.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static1/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "2d0689a2abc8a02cd4ce",
-    "url": "/static1/js/main.2d0689a2.chunk.js"
+    "revision": "bc3729c40d83c9841bc5",
+    "url": "/static1/js/main.bc3729c4.chunk.js"
   },
   {
     "revision": "04421cd3eea008be8224",
     "url": "/static1/js/1.04421cd3.chunk.js"
   },
   {
-    "revision": "2d0689a2abc8a02cd4ce",
+    "revision": "bc3729c40d83c9841bc5",
     "url": "/static1/css/main.cef774b8.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static1/css/1.73e893b4.chunk.css"
   },
   {
-    "revision": "16079e4e0faf2bfa2fb753bd5a9422dd",
+    "revision": "e2e2ab37e2cec46a1590e4b49510fd76",
     "url": "/index.html"
   }
 ];
